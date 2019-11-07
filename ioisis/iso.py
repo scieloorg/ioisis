aad5c5f8f89921fda959a43a043a9ba8db1037fe
Danilo J. S. Bellini <danilo.bellini@gmail.com>
2019-11-07 11:45:10 -0300

    Generalize terminators, line ending & ISO building
    This restructures iso.py:
    
    - Create a new classes to restream a construct accepting that the last
      chunk to be encoded isn't fully filled yet (one can simply use the
      RestreamedBuildLastIncomplete instead of Restreamed to do that),
      that's required to split the ISO into lines of a given width, since
      we can't enforce that the last line will be complete
    - Create a new create_record_struct function to create a raw (not
      restreamed) struct for a single ISO record with the given
      field/record terminators
    - Create a new line_split_restreamed function that handle the line
      breaking of the output and accept customization (new line string
      suffix/terminator fragment, and line length, not counting the
      new line string length)
    - Rename single_record_struct to DEFAULT_RECORD_STRUCT
    - Calculate the "total_len", "base_addr", and all "len" and "pos" in
      directory entries instead of requiring that the build get these as
      part of the input
    - Check if the number of fields on build match the number computed
      from base_addr
    
    With that, building an ISO from scratch got more straightforward, for
    example:
    
      >>> from ioisis.iso import DEFAULT_RECORD_STRUCT
      >>> DEFAULT_RECORD_STRUCT.build({
      ...     "dir": [{"tag": b"001"}, {"tag": b"555"}],
      ...     "fields": [b"a", b"test"],
      ... })
      b'000570000000000490004500001000200000555000500002#a#test##\n'
    
    It will split the result in lines with 80 characters (that's the
    default), but one can create another record struct to match other
    specifications, if required

--- ioisis/iso.py
+++ ioisis/iso.py
@@ -5,16 +5,21 @@ This file format specification can be found at:
 https://wiki.bireme.org/pt/img_auth.php/5/5f/2709BR.pdf
 """
 from collections import defaultdict
+from contextlib import closing
 from functools import partial
 import re
 
 from construct import Adapter, Array, Bytes, Check, CheckError, Computed, \
                       Const, Default, Embedded, Rebuild, Restreamed, \
-                      Struct, this
+                      RestreamedBytesIO, Struct, this
 
 
-FIELD_TERMINATOR = b"#"
-RECORD_TERMINATOR = b"#"
+DEFAULT_FIELD_TERMINATOR = b"#"
+DEFAULT_RECORD_TERMINATOR = b"#"
+
+# Only for building
+DEFAULT_LINE_LEN = 80
+DEFAULT_NEWLINE = b"\n"
 
 
 class IntInASCII(Adapter):
@@ -46,18 +51,83 @@ class CheckTrimSuffix(Adapter):
         return obj + self._suffix
 
 
-single_record_struct = Restreamed(
-    Struct(
+class RestreamedBytesIOWriteLastIncomplete(RestreamedBytesIO):
+    """Alternative to RestreamedBytesIO
+    that flushes the output buffer on building before closing.
+    """
+    def close(self):
+        if self.wbuffer:
+            self.substream.write(self.encoder(self.wbuffer))
+            self.wbuffer = b""
+        super(RestreamedBytesIOWriteLastIncomplete, self).close()
+
+
+class RestreamedBuildLastIncomplete(Restreamed):
+    """Alternative to Restreamed
+    that uses RestreamedBytesIOWriteLastIncomplete
+    instead of RestreamedBytesIO.
+    The difference is that this class
+    encodes the last incomplete chunk on building
+    instead of aborting the building process.
+    """
+    def _build(self, obj, stream, context, path):
+        with closing(RestreamedBytesIOWriteLastIncomplete(
+            substream=stream,
+            decoder=self.decoder,
+            decoderunit=self.decoderunit,
+            encoder=self.encoder,
+            encoderunit=self.encoderunit,
+        )) as stream2:
+            self.subcon._build(obj, stream2, context, path)
+        return obj
+
+
+def line_split_restreamed(
+    subcon,
+    line_len=DEFAULT_LINE_LEN,
+    newline=DEFAULT_NEWLINE
+):
+    """Decorates a subconstruct object
+    with something like ``construct.Restreamed``
+    to parse/build the contents in a text-like structure,
+    neglecting the CR and LF separators on parsing
+    and using the chosen newline separator on building
+    for the given line length.
+    On parsing, this doesn't check the stream splitting format,
+    it just discards the CR/LF no matter where they are.
+    """
+    size_extra = len(newline)
+    return RestreamedBuildLastIncomplete(
+        subcon,
+        decoder=partial(re.compile(b"[\r\n]").sub, b""),
+        decoderunit=1,
+        encoder=lambda chunk: chunk + newline,
+        encoderunit=line_len,
+        sizecomputer=lambda n: n + (n // line_len + 1) * size_extra,
+    )
+
+
+def create_record_struct(
+    field_terminator=DEFAULT_FIELD_TERMINATOR,
+    record_terminator=DEFAULT_RECORD_TERMINATOR,
+):
+    """Create a construct parser/builder for a whole record object."""
+    return Struct(
         # Record label/header
         Embedded(Struct(
-            "total_len" / IntInASCII(Bytes(5)),
+            "total_len" / Rebuild(IntInASCII(Bytes(5)),
+                lambda this: 26  # Label and dir/record terminators length
+                    + 13 * len(this.fields)  # Dir + field terminators length
+                    + sum(map(len, this.fields))  # Fields length
+            ),
             "status" / Default(Bytes(1), b"0"),
             "type" / Default(Bytes(1), b"0"),
             "custom_2" / Default(Bytes(2), b"00"),
             "coding" / Default(Bytes(1), b"0"),
             "indicator_count" / Default(IntInASCII(Bytes(1)), 0),
             "identifier_len" / Default(IntInASCII(Bytes(1)), 0),
-            "base_addr" / IntInASCII(Bytes(5)),
+            "base_addr" / Rebuild(IntInASCII(Bytes(5)),
+                                  lambda this: 25 + 12 * len(this.fields)),
             "custom_3" / Default(Bytes(3), b"000"),
             Embedded(Struct(  # Directory entry map
                 "len_len" / Default(IntInASCII(Bytes(1)), 4),
@@ -67,12 +137,20 @@ single_record_struct = Restreamed(
             )),
         )),
         "num_fields" / Computed((this.base_addr - 25) // 12),
+        Check(lambda this:
+            "fields" not in this or this.num_fields == len(this.fields)
+        ),
 
         # Directory
         "dir" / Struct(
             "tag" / Bytes(3),
-            "len" / IntInASCII(Bytes(this._.len_len)),
-            "pos" / IntInASCII(Bytes(this._.pos_len)),
+            "len" / Rebuild(IntInASCII(Bytes(this._.len_len)),
+                            lambda this: len(this._.fields[this._index]) +
+                                         len(field_terminator)),
+            "pos" / Rebuild(IntInASCII(Bytes(this._.pos_len)),
+                            lambda this:  # TODO: make something more efficient
+                                sum(map(len, this._.fields[:this._index])) +
+                                len(field_terminator) * this._index),
             "custom" / Rebuild(Bytes(this._.custom_len),
                                b"0" * this._.custom_len),
         )[this.num_fields],
@@ -83,26 +161,23 @@ single_record_struct = Restreamed(
                 for idx, entry in enumerate(this.dir[:-1])
             )
         )),
-        Const(FIELD_TERMINATOR),
+        Const(field_terminator),
 
         # Field data
         "fields" / Array(
             this.num_fields,
             CheckTrimSuffix(
                 Bytes(lambda this: this.dir[this._index].len),
-                FIELD_TERMINATOR,
+                field_terminator,
             ),
         ),
 
         # There should be no more data belonging to this record
-        Const(RECORD_TERMINATOR),
-    ),
-    decoder=partial(re.compile(b"[\r\n]").sub, b""),
-    decoderunit=1,
-    encoder=lambda chunk: chunk,
-    encoderunit=1,
-    sizecomputer=lambda n: n,
-)
+        Const(record_terminator),
+    )
+
+
+DEFAULT_RECORD_STRUCT = line_split_restreamed(create_record_struct())
 
 
 def con2dict(con, encoding="cp1252"):
