801585d4dd9ab9c92b0cf33239d2524d99da3846
Danilo J. S. Bellini <danilo.bellini@gmail.com>
2019-11-07 19:39:02 -0300

    Add iso2jsonl and jsonl2iso CLI commands
    These accept the standard stream as the default input/output

--- ioisis/__main__.py
+++ ioisis/__main__.py
@@ -1,7 +1,7 @@
 import click
 import ujson
 
-from . import mst
+from . import iso, mst
 
 
 @click.group()
@@ -24,6 +24,42 @@ def mst2jsonl(mst_input, jsonl_output):
             escape_forward_slashes=False,
         )
         jsonl_output.write("\n")
+
+
+@main.command()
+@click.option(
+    "iso_encoding", "--ienc",
+    default=iso.DEFAULT_ISO_ENCODING,
+    show_default=True,
+    help="ISO file encoding.",
+)
+@click.argument("iso_input", type=click.File("rb"), default="-")
+@click.argument("jsonl_output", type=click.File("w"), default="-")
+def iso2jsonl(iso_input, jsonl_output, iso_encoding):
+    """ISO2709 to JSON Lines."""
+    for record in iso.iter_records(iso_input):
+        ujson.dump(
+            record, jsonl_output,
+            ensure_ascii=False,
+            escape_forward_slashes=False,
+        )
+        jsonl_output.write("\n")
+
+
+@main.command()
+@click.option(
+    "iso_encoding", "--ienc",
+    default=iso.DEFAULT_ISO_ENCODING,
+    show_default=True,
+    help="ISO file encoding.",
+)
+@click.argument("jsonl_input", type=click.File("r"), default="-")
+@click.argument("iso_output", type=click.File("wb"), default="-")
+def jsonl2iso(jsonl_input, iso_output, iso_encoding):
+    """JSON Lines to ISO2709."""
+    for line in jsonl_input:
+        record_dict = ujson.loads(line)
+        iso_output.write(iso.dict2bytes(record_dict, encoding=iso_encoding))
 
 
 if __name__ == "__main__":
